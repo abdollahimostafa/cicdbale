ba9955ed28fea79cc4d220932bb7ba4e112f7ba1
Mostafa <mostafa@Mostafas-MacBook-Air.local>
2025-11-21 21:36:07 +0330

    aadd balejs

--- proxy.ts
+++ proxy.ts
@@ -4,16 +4,7 @@ import type { NextRequest } from "next/server";
 export function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
-  // Only protect /app routes
-  if (pathname.startsWith("/bale")) {
-    const isBale = req.cookies.get("isBale")?.value;
 
-    if (!isBale) {
-      const url = req.nextUrl.clone();
-      url.pathname = "/";
-      return NextResponse.redirect(url);
-    }
-  }
 
   // All other routes (like /not-allowed, _next, favicon) bypass middleware
   return NextResponse.next();
