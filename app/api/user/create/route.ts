4632a8c43ec3ffeef7ecfb93577c8fe74f68e46e
Mostafa <mostafa@Mostafas-MacBook-Air.local>
2025-11-22 07:59:52 +0330

    fixes

--- app/api/user/create/route.ts
+++ app/api/user/create/route.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { stringify } from "querystring";
 
 export async function POST(req: Request) {
   try {
@@ -8,6 +9,9 @@ const { baleId, phone, nationalId, inquiry } = await req.json();
 if (!baleId || !phone || !nationalId || !inquiry) {
   return NextResponse.json({ ok: false, error: "Missing parameters" }, { status: 400 });
 }
+
+  return NextResponse.json({ ok: false, error: stringify(baleId) }, { status: 400 });
+
 
 const user = await prisma.user.create({
   data: {
@@ -23,6 +27,6 @@ const user = await prisma.user.create({
 });    return NextResponse.json({ ok: true, user });
   } catch (error) {
     console.error("Register user error:", error);
-    return NextResponse.json({ ok: false, error: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json({ ok: false, error: baleId}, { status: 500 });
   }
 }
